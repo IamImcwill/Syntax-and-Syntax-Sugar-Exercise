c2491a6cb9fd648312ebe856d344d9e831c1c933
Caris Williams <cwill1013@gmail.com>
2024-01-16 19:28:10 -0500

    initial commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -3,7 +3,25 @@
     public class Program
     {
         static void Main(string[] args)
+
+        //     int answer = 4;
+        //string response;
+        //if (answer< 9) 
+        //{
+        //    response = answer + " is less than nine";
+        //}
+        //else
+        //{
+        //    response = answer + " is greater than or equal to nine";
+
+
         {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine"
+                : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+            //Console.WriteLine("Hello, World!");
+
             Console.WriteLine("Hello, World!");
         }
     }
